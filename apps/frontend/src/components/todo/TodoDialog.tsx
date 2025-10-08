6028390420c23bf36a8585722fad63b327602da0
aliyarerbolatov12-maker <aliyarerbolatov12@gmail.com>
2025-10-08 21:21:22 +0500

    feat(frontend/todo): integrate SelectCategories component into task form

--- apps/frontend/src/components/todo/TodoDialog.tsx
+++ apps/frontend/src/components/todo/TodoDialog.tsx
@@ -14,6 +14,7 @@ import React, { useState } from "react";
 import type { TodoItemProps } from "@/types/todo.types";
 import { Category } from "@/constant/category.constant";
 import { Button } from "@/components/ui/button";
+import { SelectCategories } from "./SelectCategories";
 
 interface TodoProps {
   onSave: (todo: TodoItemProps) => void;
@@ -101,6 +102,15 @@ export default function TodoDialog({ onSave }: TodoProps) {
             placeholder="Description"
             value={task.description}
             onChange={handleTaskChange}
+          />
+
+          <SelectCategories
+            onChange={(category) =>
+              setTask((prev) => ({
+                ...prev,
+                category,
+              }))
+            }
           />
 
           <div className="flex flex-col gap-2">
