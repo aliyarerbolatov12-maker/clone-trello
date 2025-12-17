2f98e28986b107e9b65e0c2fa6299e1fa0d2bffc
aliyarerbolatov12-maker <aliyarerbolatov12@gmail.com>
2025-12-17 22:21:27 +0500

    feat(task store): add persist functionality for task data storage

--- apps/frontend/src/store/task/useTasks.store.ts
+++ apps/frontend/src/store/task/useTasks.store.ts
@@ -1,30 +1,38 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import type { TasksState } from "@/types/store/taskStore.types";
 
-export const useTasksStore = create<TasksState>((set) => ({
-  tasks: [],
+export const useTasksStore = create<TasksState>()(
+  persist(
+    (set) => ({
+      tasks: [],
 
-  addTask: (task) =>
-    set((state) => ({
-      tasks: [...state.tasks, { ...task, id: crypto.randomUUID() }],
-    })),
+      addTask: (task) =>
+        set((state) => ({
+          tasks: [...state.tasks, { ...task, id: crypto.randomUUID() }],
+        })),
 
-  deleteTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.filter((t) => t.id !== id),
-    })),
+      deleteTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.filter((t) => t.id !== id),
+        })),
 
-  editTask: (updatedTask) =>
-    set((state) => ({
-      tasks: state.tasks.map((t) =>
-        t.id === updatedTask.id ? updatedTask : t
-      ),
-    })),
+      editTask: (updatedTask) =>
+        set((state) => ({
+          tasks: state.tasks.map((t) =>
+            t.id === updatedTask.id ? updatedTask : t
+          ),
+        })),
 
-  toggleCompleted: (id) =>
-    set((state) => ({
-      tasks: state.tasks.map((t) =>
-        t.id === id ? { ...t, completed: !t.completed } : t
-      ),
-    })),
-}));
+      toggleCompleted: (id) =>
+        set((state) => ({
+          tasks: state.tasks.map((t) =>
+            t.id === id ? { ...t, completed: !t.completed } : t
+          ),
+        })),
+    }),
+    {
+      name: "tasks-storage",
+    }
+  )
+);
